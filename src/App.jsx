27af621e5364ddd89b9a669c0a381316bbe64de9
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-11-10 16:18:20 -0500

    create the logic for isTopOfPage with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px");
+
+  useEffect(() => {
+    const handleScroll = () => {
+     if(window.scrollY === 0) isTopOfPage(true)
+     if(window.scrollY !== 0) isTopOfPage(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className="w-5/6 mx-auto md:h-full">
+        
+      </div>
     </div>
   );
 }
