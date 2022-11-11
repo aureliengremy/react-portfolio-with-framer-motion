3c8ad9e7db8fab58ce686951ecd1913d752881ef
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-11-11 14:26:47 -0500

    add image of myself in public folder/assets - change hover effect on picture

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,21 +15,21 @@ const Landing = ({ setSelectedPage }) => {
         {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-20 before:absolute before:-top-20 
-                before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] 
+                before:-left-20 before:rounded-t-[400px] before:rounded-b-[400px] before:w-full before:max-w-[400px] 
                 before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
-              src="../assets/profile-image.png"
+              src="../assets/profile-heigh-full.jpg"
               alt="profil"
-              className="hover:filter hover:saturate-200 transition duration-500 z-10
+              className="hover:filter rounded-t-[400px] rounded-b-[400px] hover:hue-rotate-30 hover:contrast-125 transition duration-500 z-10
                     w-full max-w-[400px] md:max-w-[600px]"
             />
           </div>
         ) : (
           <img
-            src="../assets/profile-image.png"
+            src="../assets/profile-heigh-full.jpg"
             alt="profil"
-            className="hover:filter hover:saturate-200 transition duration-500 z-10
+            className="hover:filter rounded-t-[400px] rounded-b-[400px] hover:hue-rotate-30 hover:contrast-125 transition duration-500 z-10
                     w-full max-w-[400px] md:max-w-[600px]"
           />
         )}
