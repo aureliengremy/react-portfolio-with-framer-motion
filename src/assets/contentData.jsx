7de9227cf4dbf3c4cce20dc41c5e7d810b328a79
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-11-25 19:00:03 -0500

    createContext to provide the language from the navigator - create ContentData

---
+++ src/assets/contentData.jsx
@@ -0,0 +1,14 @@
+const data = {
+    FR: {
+        landing: {
+            description: "Titulaire d’une maîtrise en e-Business et e-Marketing, complétée par ma passion pour Internet et l’informatique. Curieux de toujours comprendre au mieux le fonctionnement des technologies du web."
+        }
+    },
+    EN: {
+        Landing: {
+            description: ""
+        }
+    }
+}
+
+export default data
---
+++ src/context/langContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from "react";
+
+export const Context = createContext();
+
+const supportedLangs = ["EN", "FR", "ES"];
+
+let browserLang = navigator.language.slice(0, 2).toUpperCase();
+console.log(browserLang);
+
+if (supportedLangs.indexOf(browserLang) === -1) {
+  console.log("Not supported");
+  browserLang = "EN";
+}
+
+const ContextProvider = (props) => {
+  const [lang, setLang] = useState(browserLang);
+
+  const toggleLang = (changeLang) => {
+    setLang(changeLang);
+  };
+
+  return (
+    <Context.Provider value={{ lang, toggleLang }}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+export default ContextProvider;
