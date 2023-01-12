6fcfceb25e9f398d1a5c0865000b8aaf47b425a4
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-01-12 17:48:49 -0500

    align center all element - Footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <footer className="pt-5">
     <div className="w-5/6 mx-auto">
         <div className="md:flex justify-center md:justify-between text-center">
-            <p className="font-playfair font-semibold text-2xl text-white my-10">
+            <p className="flex items-center font-playfair font-semibold text-2xl text-white my-10">
                 AURELIEN GREMY
             </p>
         <SocialMediaIcons />
-            <p className="font-playfair text-md text-white my-10">
+            <p className="flex items-center font-playfair text-md text-white my-10">
                 ©2023. All Rights Reserved
                 </p>
         </div>
